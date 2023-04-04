7d93ac548407233a2be49f3caaa3909c3196bff9
Mohamed <amiiini44@hotmail.com>
2023-04-04 01:27:47 -0500

    updated JS

--- script.js
+++ script.js
@@ -1,7 +1,52 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+var generateBtn = document.querySelector("#generate");
+var specialcharacters = [ "!", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~", ];
+var letter = ["a","b","c","d","e","f","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var num = ["0","1","2","3","4","5","6","7","8","9",];
+var users = [""]
+
+
+function generatePassword() {
+  var passLength = parseInt(prompt("How many characters would you like?"));
+  if(passLength < 8){
+    alert("Password must be at least 8");
+    return generatePassword()}
+  
+      
+  if(passLength > 128){
+    alert("Password most not go byond 128 characters");
+    return generatePassword()
+  }
+    {
+      var upper= confirm("Include upper case letters?")
+      var lower= confirm("Include lower case letters?")
+      var special= confirm("Include special characters?")
+      var number= confirm("Include numbers?")
+
+    }
+    
+    if(upper){
+      users += upper
+    }
+    if(lower){
+      users += lower
+    }
+  
+    if(special) {
+      users += special
+    }
+  
+    if(number) {
+      users += number
+    }
+    
+for (var i = 0; i < passLength; i++) {
+  result += users.charAt(Math.floor(Math.random() * users.passLength));
+}
+return result;
+
+}
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -9,6 +54,5 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
